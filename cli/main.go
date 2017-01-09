bd9bed85aa169bc3d9222848f4cb56ccc9fa385a
Adam Jacob Muller <adam@isprime.com>
2017-01-08 21:58:52 -0500

    use abstracted controller

--- cli/main.go
+++ cli/main.go
@@ -1,22 +1,18 @@
 package main
 
 import (
+	"github.com/AdamJacobMuller/home-api/api/controller"
 	"github.com/AdamJacobMuller/home-api/api/server"
 	"github.com/AdamJacobMuller/home-api/providers/homeseer"
 )
 
 func main() {
-	controller := homeseer.NewHomeseerController("http://homeseer.adam.gs")
+	controller := apicontroller.NewAPIController()
+	hscontroller := homeseer.NewHomeseerController("http://homeseer.adam.gs")
 	api := apiserver.NewAPIServer()
-	api.HSController = controller
+
+	controller.AddProvider(hscontroller)
+	api.Controller = controller
 
 	api.Serve()
-	/*
-		controller.SetDevicesValue(homeseer.HSLookup{"LocationOne": "Living Room", "TypeString": "Z-Wave Switch Binary"}, 0)
-		controller.SetChildDevicesValue(homeseer.HSLookup{"LocationOne": "Living Room", "Child": homeseer.HSLookup{"Name": "Color Control Warm_White Channel"}}, 255)
-		controller.SetChildDevicesValue(homeseer.HSLookup{"LocationOne": "Living Room", "Child": homeseer.HSLookup{"Name": "Color Control Cold_White Channel"}}, 0)
-		controller.SetChildDevicesValue(homeseer.HSLookup{"LocationOne": "Living Room", "Child": homeseer.HSLookup{"Name": "Color Control Red Channel"}}, 0)
-		controller.SetChildDevicesValue(homeseer.HSLookup{"LocationOne": "Living Room", "Child": homeseer.HSLookup{"Name": "Color Control Blue Channel"}}, 0)
-		controller.SetChildDevicesValue(homeseer.HSLookup{"LocationOne": "Living Room", "Child": homeseer.HSLookup{"Name": "Color Control Green Channel"}}, 0)
-	*/
 }
