a5b89206871b70f849408ec01585490013619ee6
Adam Jacob Muller <adam@isprime.com>
2017-01-09 20:51:36 -0500

    implement device and devices interfaces

--- api/models/models.go
+++ api/models/models.go
@@ -3,6 +3,19 @@ package apimodels
 type Match map[string]interface{}
 
 type ControlRequest struct {
-	Match Match   `json:"match"`
-	Value float64 `json:"value"`
+	Match  Match   `json:"match"`
+	Value  float64 `json:"value,omitempty"`
+	Action string  `json:"action,omitempty"`
+}
+
+type Device interface {
+	SetValue(float64) bool
+	InvokeAction(string) bool
+	IDString() string
+	Matches(Match) bool
+}
+
+type Devices interface {
+	SetValue(float64) bool
+	InvokeAction(string) bool
 }
