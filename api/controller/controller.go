c9af2b3be22c58a7cb90945cdb22dcf8e0feb93f
Adam Jacob Muller <adam@isprime.com>
2017-01-09 20:50:24 -0500

    add the ability to invoke actions on devices and child devices

--- api/controller/controller.go
+++ api/controller/controller.go
@@ -7,6 +7,10 @@ import (
 type ControllerProvider interface {
 	SetDevicesValue(apimodels.Match, float64) bool
 	SetChildDevicesValue(apimodels.Match, float64) bool
+
+	InvokeDevicesAction(apimodels.Match, string) bool
+	InvokeChildDevicesAction(apimodels.Match, string) bool
+	IDString() string
 }
 
 type APIController struct {
@@ -23,6 +27,19 @@ func (c *APIController) SetDevicesValue(match apimodels.Match, value float64) bo
 func (c *APIController) SetChildDevicesValue(match apimodels.Match, value float64) bool {
 	for _, provider := range c.providers {
 		provider.SetChildDevicesValue(match, value)
+	}
+	return true
+}
+
+func (c *APIController) InvokeChildDevicesAction(match apimodels.Match, action string) bool {
+	for _, provider := range c.providers {
+		provider.InvokeChildDevicesAction(match, action)
+	}
+	return true
+}
+func (c *APIController) InvokeDevicesAction(match apimodels.Match, action string) bool {
+	for _, provider := range c.providers {
+		provider.InvokeDevicesAction(match, action)
 	}
 	return true
 }
