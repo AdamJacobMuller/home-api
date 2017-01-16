be682f29a081024424c2b79024e042746e5546a4
Adam Jacob Muller <adam@isprime.com>
2017-01-15 22:21:12 -0500

    load configuration using flag

--- cli/api/main.go
+++ cli/api/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/AdamJacobMuller/home-api/api/server"
+	"github.com/namsral/flag"
+	"os"
 )
 
 func main() {
+	var configuration string
+	var providers_d string
+
+	flag.StringVar(&configuration, "configuration", "", "configuration file")
+	flag.StringVar(&providers_d, "providers-d", "", "directory containing provider json configuration files")
+	flag.Parse()
+
+	if configuration == "" {
+		fmt.Printf("-configuration is required\n")
+		os.Exit(1)
+	}
+
 	api := apiserver.NewAPIServer()
-
-	api.LoadAndCreateProviders("config.json")
-
+	api.LoadAndCreateProviders(configuration)
 	api.Serve()
 }
