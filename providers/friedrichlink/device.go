7f3b5c9d750de6933428bb1c7d06ed41dfc71eee
Adam Jacob Muller <adam@isprime.com>
2017-07-24 03:07:35 -0400

    FriedrichLink device shell

---
+++ providers/friedrichlink/device.go
@@ -0,0 +1,51 @@
+package friedrichlink
+
+import (
+	"github.com/AdamJacobMuller/home-api/api/models"
+)
+
+type FriedrichLinkDevice struct {
+}
+
+func (d *FriedrichLinkDevice) HasChildDevice(find apimodels.Match) bool {
+	return false
+}
+func (d *FriedrichLinkDevice) GetChildDevice(find apimodels.Match) apimodels.Device {
+	return &FriedrichLinkDevice{}
+}
+func (d *FriedrichLinkDevice) IsHidden() bool {
+	return false
+}
+func (d *FriedrichLinkDevice) ListChildren() []apimodels.Device {
+	return []apimodels.Device{}
+}
+func (d *FriedrichLinkDevice) ListActions() []apimodels.Action {
+	return []apimodels.Action{}
+}
+func (d *FriedrichLinkDevice) ListTypes() []apimodels.Type {
+	return []apimodels.Type{}
+}
+func (d *FriedrichLinkDevice) SetValue(float64) bool {
+	return false
+}
+func (d *FriedrichLinkDevice) InvokeAction(string) bool {
+	return false
+}
+func (d *FriedrichLinkDevice) GetName() string {
+	return "FriedrichLinkDeviceName"
+}
+func (d *FriedrichLinkDevice) GetLocationOne() string {
+	return "Floor"
+}
+func (d *FriedrichLinkDevice) ProviderIDString() string {
+	return "FriedrichLinkID"
+}
+func (d *FriedrichLinkDevice) GetLocationTwo() string {
+	return "Room"
+}
+func (d *FriedrichLinkDevice) IDString() string {
+	return "FriedrichLinkDeviceID"
+}
+func (d *FriedrichLinkDevice) Matches(apimodels.Match) bool {
+	return false
+}
