857b7d44c6d3124e03cf9debbc25015c7492c9b6
Adam Jacob Muller <adam@isprime.com>
2017-07-25 02:07:32 -0400

    implement device listing stuff

--- providers/friedrichlink/list.go
+++ providers/friedrichlink/list.go
@@ -5,14 +5,22 @@ import (
 )
 
 type FriedrichLinkList struct {
+	devices []*FriedrichLinkDevice
 }
 
 func (l *FriedrichLinkList) SetValue(float64) bool {
 	return false
 }
 func (l *FriedrichLinkList) List() []apimodels.Device {
-	return []apimodels.Device{}
+	var v []apimodels.Device
+	for _, i := range l.devices {
+		v = append(v, i)
+	}
+	return v
 }
 func (l *FriedrichLinkList) InvokeAction(string) bool {
 	return false
+}
+func (l *FriedrichLinkList) Add(device *FriedrichLinkDevice) {
+	l.devices = append(l.devices, device)
 }
